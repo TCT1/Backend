a8f3f7556a77c817fc1174c1330e04e4e9f284aa
Leonardo JV <leonjv08@gmail.com>
2023-01-22 19:37:51 -0600

    Implementacion de socketio (Hay un error)

--- src/apps.js
+++ src/apps.js
@@ -1,16 +1,18 @@
 import express from "express";
-import handlebars from "express-handlebars"
-import path from "path"
 import __dirname from "./utils.js";
+import handlebars from "express-handlebars"
+import viewsRouter from "./routes/views.router.js";
+import {Server} from 'socket.io'
 
 const app = express()
+const httpServer=app.listen(8080,()=>console.log("Server arriba"));
+const io = new Server(httpServer)
 
 app.engine("handlebars", handlebars.engine())
-
-app.set('views',__dirname + "/views")
-app.set("view engine","handlebars")
-
-app.use(express.static(path.join(__dirname,'views')))
+app.set('views',__dirname + '/views')
+app.set('view engine',"handlebars")
+app.use(express.static(__dirname + '/public'));
+app.use('/',viewsRouter);
 
 let products = []
 
@@ -83,9 +85,6 @@ productos.addProduct(
     20
 )
 
-app.get('/',(req,res)=>{
-    let testUser={name:"Leonardo",lastname:"Juventino",role:"admin"}
-    res.render('index', {user:testUser,isAdmin:testUser.role==="admin",products})
+io.on('connection',socket=>{
+    console.log("Tenemos un cliente conectado");
 })
-
-const server=app.listen(8080,()=>console.log("server arriba"))
